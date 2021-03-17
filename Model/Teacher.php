587221e511804cde6b871b3ff63650455142e509
Oscar <oscarpete@gmail.com>
2021-03-17 16:06:19 +0100

    teacher model added

--- Model/Teacher.php
+++ Model/Teacher.php
@@ -4,5 +4,39 @@ include_once "Entity.php";
 
 class Teacher extends Entity
 {
+    private int $id;
+    private string $firstName;
+    private string $lastName;
+    private string $email;
+
+    public function __construct(int $id, string $firstName, string $lastName, string $email)
+    {
+        $this->id = $id;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->email = $email;
+
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getfirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function getlastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
 
 }
